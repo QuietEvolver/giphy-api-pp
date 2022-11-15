d7af057ed235904b9764622a88f39dc1800d0e2b
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-15 15:46:43 -0800

    It will add click button endpoint to url calls.
    Co-authored-by: Henry Sullivan <sullivanbhenry@gmail.com>.

--- src/index.js
+++ src/index.js
@@ -7,13 +7,8 @@ import './assets/css/styles.css';
 
 function getGiphy(search) {
   let request = new XMLHttpRequest();
-  // const urlGiphy = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${process.env.API_KEY}&limit=5`;
-
-  //javascript, jQuery
 
 const xhr = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${process.env.API_KEY}&limit=5`;
-// xhr.done(function(data) { console.log("success got data", data); })
-            
 
   request.addEventListener("loadend", function() {
     const response = JSON.parse(this.responseText);
@@ -29,19 +24,42 @@ const xhr = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${process.e
   request.send();
 }
 
+function getTrending() {
+  let request = new XMLHttpRequest();
+
+const url = `http://api.giphy.com/v1/gifs/trending?&api_key=${process.env.API_KEY}`;
+
+  request.addEventListener("loadend", function() {
+    const response = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      printTrending(response);
+      console.log("success got data", response); 
+    } else {
+      printError(this, response);
+    }
+  });
+
+  request.open("GET", url, true);
+  request.send();
+}
+
 // UI Logic
 
 function printElements(apiResponse) {
-  // document.querySelector('#showResponse').innerText = `The ${apiResponse.data[1].embed_url}pics in ${apiResponse.data[2].images.looping.mp4} is ${apiResponse.data[2].url}`;
 
   let imgElement = document.createElement('img')
   imgElement.src = apiResponse.data[0].images.original.url;
   document.querySelector('body').append(imgElement);
 }
-// ${apiResponse.data[2].images.original.url}
 
-function printError(request, apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+function printTrending(apiResponse) {
+  let imgElement = document.createElement('img')
+  imgElement.src = apiResponse.data[0].images.original.url;
+  document.querySelector('body').append(imgElement);
+}
+
+function printError(request, apiResponse) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the trending gifs ${request.status} ${request.statusText}: ${apiResponse.message}`;
 }
 
 function handleFormSubmission(event) {
@@ -50,7 +68,12 @@ function handleFormSubmission(event) {
   document.querySelector('#location').value = null;
   getGiphy(search);
 }
-
+function handleTrendingClick(){
+  // e.preventDefault();
+  // const trending = document.getElementById('a href =`https://api.giphy.com/v1/gifs/trending?api_key=jjPQb6Tt61D8XYDEzY0Au0gUoIx4r7z4&limit=25&rating=g`') 
+  getTrending();
+}
 window.addEventListener("load", function() {
   document.querySelector('form').addEventListener("submit", handleFormSubmission);
+  document.querySelector("button#trending-button").addEventListener("click", handleTrendingClick);
 });
